8aee028f76168c32c9bd60d30a1f4f68805c5b65
ifiokudoidiok <ifiok_udoidiok@yahoo.com>
2019-11-15 09:57:57 +0100

    implemented token Generator function; Register endpoint completed

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,11 +1,47 @@
-const router = require('express').Router();
+const router = require("express").Router();
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
-router.post('/register', (req, res) => {
+const db = require("../database/db-model");
+
+router.post("/register", register);
+
+router.post("/login", login);
+
+function register(req, res) {
   // implement registration
-});
+  const { username, password } = req.body;
+  const hash = bcrypt.hashSync(password, 12);
 
-router.post('/login', (req, res) => {
+  const newUser = {
+    username: username,
+    password: hash
+  };
+
+  db.register(newUser).then(credentials => {
+    const token = generateToken(newUser);
+    res.status(201).json({ credentials, token: token });
+  });
+}
+
+function login(req, res) {
   // implement login
-});
+}
+
+function generateToken(user) {
+  const payload = {
+    sub: user.id,
+    username: user.username
+  };
+  const options = {
+    expiresIn: "1d"
+  };
+  const result = jwt.sign(
+    payload,
+    "This is secret, Keep it safe, keep it secure",
+    options
+  );
+  return result;
+}
 
 module.exports = router;
