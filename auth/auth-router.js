dcd5801ddc1003052717d0dbad5a51bb0b4c38bf
ifiokudoidiok <ifiok_udoidiok@yahoo.com>
2019-11-15 10:03:00 +0100

    implemented login router

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -26,6 +26,24 @@ function register(req, res) {
 
 function login(req, res) {
   // implement login
+  const { username, password } = req.body;
+  db
+    .login({ username })
+    .first()
+    .then(user => {
+      if (user && bcrypt.compareSync(password, user.password)) {
+        const token = generateToken(user);
+        res.status(200).json({
+          message: `Welcome ${user.username}!`,
+          token: token
+        });
+      } else {
+        res.status(401).json({ message: "Invalid Credentials" });
+      }
+    })
+    .catch(error => {
+      res.status(500).json({ message: error.message });
+    });
 }
 
 function generateToken(user) {
